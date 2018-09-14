96ac2ecbd8a50f33ca293e67bc52831c3d7a64c6
Jeremy Morse <jgmorse@umich.edu>
2018-09-14 18:31:18 -0400

    Helps resolve HELIO-2106

--- spec/turnsole/heliotrope_service_spec.rb
+++ spec/turnsole/heliotrope_service_spec.rb
@@ -2,21 +2,35 @@
 RSpec.describe Turnsole::HeliotropeService do
   heliotrope_service = described_class.new
 
+  products_initial_count = heliotrope_service.products.count
+  institutions_initial_count = heliotrope_service.institutions.count
+  lessees_initial_count = heliotrope_service.lessees.count
   n = 3
   products = []
+  institutions = []
   lessees = []
   components = []
 
   products_initial_count = heliotrope_service.products.count
   lessees_initial_count = heliotrope_service.lessees.count
+  institutions_initial_count = heliotrope_service.institutions.count
   components_initial_count = heliotrope_service.components.count
 
   before(:all) do
     n.times do |i|
       products << "product#{i}"
       heliotrope_service.find_or_create_product(identifier: products[i])
-      lessees << "lessee#{i}@example.com"
-      heliotrope_service.find_or_create_lessee(identifier: lessees[i])
+
+      inst['inst_id'] = "instid_#{i}"
+      inst['inst_name'] = "Institution #{i}"
+      inst['entity_id'] = "entity_id_#{i}" if i.odd?  #test that field is optional
+      institutions << inst
+      if inst['entity_id']
+          heliotrope_service.find_or_create_institution(identifier: inst['inst_id'], name: inst['inst_name'], entity_id: inst['entity_id'])
+      else
+          heliotrope_service.find_or_create_institution(identifier: inst['inst_id'], name: inst['inst_name'] )
+      end
+      lessees << inst['inst_id']
       components << "component#{i}"
       heliotrope_service.find_or_create_component(handle: components[i])
     end
@@ -32,12 +46,14 @@ RSpec.describe Turnsole::HeliotropeService do
     n.times do |i|
       heliotrope_service.delete_product(identifier: products[i])
       heliotrope_service.delete_lessee(identifier: lessees[i])
+      heliotrope_service.delete_institution(identifier: lessees[i])
       heliotrope_service.delete_component(handle: components[i])
     end
   end
 
   it 'works' do
     expect(heliotrope_service.products.count).to eq(products_initial_count + n)
+    expect(heliotrope_service.institutions.count).to eq(institutions_initial_count + n)
     expect(heliotrope_service.lessees.count).to eq(lessees_initial_count + n)
     expect(heliotrope_service.components.count).to eq(components_initial_count + n)
 
@@ -49,6 +65,11 @@ RSpec.describe Turnsole::HeliotropeService do
     end
 
     n.times do |i|
+      if institutions[i]['entity_id']
+        heliotrope_service.find_or_create_institution(identifier: institutions[i]['inst_id'], name: institutions[i]['inst_name'], entity_id: institutions[i]['entity_id'])
+      else
+        heliotrope_service.find_or_create_institution(identifier: institutions[i]['inst_id'], name: institutions[i]['inst_name'])
+      end
       heliotrope_service.link(product_identifier: products[0], lessee_identifier: lessees[i])
     end
     expect(heliotrope_service.product_lessees(product_identifier: products[0]).count).to eq(n)
